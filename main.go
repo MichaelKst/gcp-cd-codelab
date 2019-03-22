b0515597909f0d4691d16a22fedcd68dc4fb79f8
Michael <michael.ly.hc@gmail.com>
2019-03-22 16:35:27 +0100

    changed bg color to pink

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<body style='background-color: pink'><h1>Hello World!</h1></body>")
+	io.WriteString(w, "<body style='background-color: #9ae1ec'><h1>Hello World!</h1></body>")
 }
 
 func main() {
