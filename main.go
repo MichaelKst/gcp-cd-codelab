0b2bc6ed2a8a36868544f7a742e9574b03eafe42
Michael <michael.ly.hc@gmail.com>
2019-03-26 14:53:32 +0100

    changed bg color to pale light purple

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<body style='background-color: #ffe595'><h1>Hello World!</h1></body>")
+	io.WriteString(w, "<body style='background-color: #d2b9db'><h1>Hello World!</h1></body>")
 }
 
 func main() {
