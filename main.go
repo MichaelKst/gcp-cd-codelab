aeda1b46d4131b340901890f112f4a944cc3a4d7
MichaelKst <47251704+MichaelKst@users.noreply.github.com>
2019-07-23 15:14:54 +0200

    Update main.go

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func linty(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", linty)
-	http.Handle("/image/Linty6.jpg", http.FileServer(http.Dir("image/")))
+	http.Handle("/image/", http.StripPrefix("/image/", http.FileServer(http.Dir("image"))))
+	//http.Handle("/image/Linty6.jpg", http.FileServer(http.Dir("image/")))
 	http.ListenAndServe(":5000", nil)
 }
