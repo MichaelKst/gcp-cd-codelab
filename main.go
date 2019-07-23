09ff92804306501da84ff37078d7b577e6cd0360
MichaelKst <47251704+MichaelKst@users.noreply.github.com>
2019-07-23 14:48:41 +0200

    Update main.go

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func linty(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<html><body><img src='http://localhost:5000/Linty6.jpg'></body></html>")
+	fmt.Fprintf(w, "<html><body><img src='http://localhost:5000/image/Linty6.jpg'></body></html>")
 }
 
 func main() {
 	http.HandleFunc("/", linty)
-	http.Handle("/Linty6.jpg", http.FileServer(http.Dir("image")))
+	http.Handle("/image/Linty6.jpg", http.FileServer(http.Dir("image/")))
 	http.ListenAndServe(":5000", nil)
 }
