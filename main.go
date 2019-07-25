a5c39bf42e40ff6169c2f8987d697eaf410cf904
MichaelKst <47251704+MichaelKst@users.noreply.github.com>
2019-07-25 14:10:57 +0200

    Update main.go

--- main.go
+++ main.go
@@ -6,41 +6,7 @@ import (
 )
 
 func linty(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w,
-		    "
-		    <!DOCTYPE html>
-		    <html lang='en'>
-		    <head>
-			<meta charset="utf-8">
-    			<title>Linty Services</title>
-    			<meta charset="utf-8">
-    			<meta name="viewport" content="width=device-width, initial-scale=1">
-			
-		    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-		    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-    			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>	
-		    
-		    	<style>
-		    		.image {
-					background-color: #999;
-    					width: 50%;
-    					height: 50%;
-    					display: inline-block;
-		    		}
-		    		.image img {
-					width:100%;
-		    		}
-		    	</style>
-		    
-		    	<body>
-		    		<div class='image'>
-		    		<img src='https://www.aboriva.com/img/couv/34/nm_sugar-num178-juillet-aout-2016_1154.jpg'>
-		    		<img src='image/Linty6.jpg'>
-		    		</div>
-		    	</body>
-		    </html>
-		    ")
+	fmt.Fprintf(w,"<!DOCTYPE html><html lang='en'><head><meta charset="utf-8"><title>Linty Services</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script><style>.image {background-color: #999;width: 50%;height: 50%;display: inline-block;}.image img {width:100%;}</style><body><div class='image'><img src='https://www.aboriva.com/img/couv/34/nm_sugar-num178-juillet-aout-2016_1154.jpg'><img src='image/Linty6.jpg'></div></body></html>")
 }
 
 func main() {
