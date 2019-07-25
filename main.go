798efbbd974fbf51aa8ecf8299bdd67c43b815bf
MichaelKst <47251704+MichaelKst@users.noreply.github.com>
2019-07-25 11:09:53 +0200

    Update main.go
    Test to load an image available on the web

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 )
 
 func linty(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<html><body><img src='/image/Linty6.jpg'></body></html>")
+	fmt.Fprintf(w, "<html><body><img src='https://www.aboriva.com/img/couv/34/nm_sugar-num178-juillet-aout-2016_1154.jpg'></body></html>")
 }
 
 func main() {
 	http.HandleFunc("/", linty)
-	http.Handle("/image/", http.StripPrefix("/image/", http.FileServer(http.Dir("go/bin/gcp-cd-codelab/image"))))
+	//http.Handle("/image/", http.StripPrefix("/image/", http.FileServer(http.Dir("go/bin/gcp-cd-codelab/image"))))
 	//http.Handle("/image/Linty6.jpg", http.FileServer(http.Dir("image/")))
 	//http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	http.ListenAndServe(":5000", nil)
