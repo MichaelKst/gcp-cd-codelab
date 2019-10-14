5ca005dfb1a575588c58ed257e01a4e04426aaaf
MichaelKst <47251704+MichaelKst@users.noreply.github.com>
2019-10-14 16:35:40 +0200

    Update main.go
    ISCLP logo avec fond transparent

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func linty(w http.ResponseWriter, r *http.Request) {
 	// Print the HTML content of the web page to be displayed
-	fmt.Fprintf(w,"<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><title>Linty Services</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css'><script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js'></script><script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js'></script><style>.image img {width:200px;display: block;margin-left: auto;margin-right: auto;}</style><body style='background-color:#edf1ff;'><div class='image'><img src='image/ISCLP.jpg'></div></body></html>")
+	fmt.Fprintf(w,"<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><title>Linty Services</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css'><script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js'></script><script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js'></script><style>.image img {width:200px;display: block;margin-left: auto;margin-right: auto;}</style><body style='background-color:#edf1ff;'><div class='image'><img src='image/ISCLP-fondtransparent.jpg'></div></body></html>")
 }
 
 func main() {
